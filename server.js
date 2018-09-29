ed41cdba2fdbba60f980e4039af2422870899afc
n3rdgir1 <nerdgirl11235813@gmail.com>
2018-09-29 13:34:10 -0500

    just use the dist directory, not app/dist to serve app

--- server.js
+++ server.js
@@ -20,10 +20,10 @@ app.use(forceSSL());
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '../dist'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '../dist/index.html'));
 });
 
 // Start the app by listening on the default
